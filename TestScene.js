644bf08c82bc06ab17c8b38efdc0287b072413b5
jbbennett0916 <78577680+jbbennett0916@users.noreply.github.com>
2021-03-20 22:27:23 -0500

    Add files via upload

--- TestScene.js
+++ TestScene.js
@@ -1,3 +1,4 @@
+
 class TestScene extends Phaser.Scene {
     constructor() {
         super("TestScene");
@@ -5,24 +6,25 @@ class TestScene extends Phaser.Scene {
         this.path = null;
         this.spawnedThisWave = 0;
         this.spawnInterval = null;
+        this.shootInterval = null;
         this.findBaddie = 0;
+        this.placedHeroes = [];
+        this.spawnedBaddies = [];
         this.baddieList = [
             {
                 "key": "ancient",
-                "duration": 30000,
-                "health": 150
+                "duration": 30000
             },
             {
                 "key": "spider_forest",
-                "duration": 15000,
-                "health": 80
+                "duration": 15000
             },
             {
                 "key": "roboost",
-                "duration": 20000,
-                "health": 100
+                "duration": 20000
             }
         ];
+
         this.goodieList = [
             {
                 "key": "purple_guy",
@@ -36,73 +38,80 @@ class TestScene extends Phaser.Scene {
                 "key": "green_guy",
                 "damage": 20
             }
+
         ];
     }
     preload() {
         //bad guys
-        this.load.image('ancient', 'Images/ancient.png');
-        this.load.image('spider_forest', 'Images/spider_forest.png');
-        this.load.image('roboost', 'Images/roboost007.png');
+        this.load.image('ancient', 'ancient.png');
+        this.load.image('spider_forest', 'spider_forest.png');
+        this.load.image('roboost', 'roboost007.png');
 
         //good guys
-        this.load.image('purple_guy', 'Images/enemy1idle1.png');
-        this.load.image('dual_wield', 'Images/enemy2idle1.png');
-        this.load.image('green_guy', 'Images/enemy3idle1.png');
+        this.load.image('purple_guy', 'enemy1idle1.png');
+        this.load.image('dual_wield', 'enemy2idle1.png');
+        this.load.image('green_guy', 'enemy3idle1.png');
 
+        //bullets
+        this.load.image('green_bullet', 'bullets1_0023_Circle------------------.png')
 
     }
     create() {
-        //this graphics element is only for visualization,
-        //its not related to our path
+        // this graphics element is only for visualization, 
+        // its not related to our path
         var pathGraphics = this.add.graphics();
         var gridGraphics = this.add.graphics();
-
+        //this.path = this.add.path
         this.createPath();
+        //draws the path
         this.drawPath(pathGraphics);
-
+        //draws a grid
         this.drawGrid(gridGraphics);
 
-        // the path for our enemies
-        // parameters are the start x and y of our path
-        //this.path = this.add.path(96, -32);
-        //this.path.lineTo(96, 164);
-        //this.path.lineTo(480, 164);
-        //this.path.lineTo(480, 544);
-        //this.path.lineTo(800, 544);
-        //this.path.lineTo(800, 350);
-        //this.path.lineTo(1200, 350);
-        //this.path.lineTo(1200, 650);
-        //this.path.lineTo(1400, 650);
-        //this.path.lineTo(1400, 900);
-
-        //graphics.lineStyle(10, 0x12ff0a, 0.5);
-        // visualize the path
-        //this.path.draw(graphics);
 
 
         //adds enemies
 
-        this.addEnemy('spider_forest', 15000, 80);
-        this.addEnemy('roboost', 20000, 100);
-        this.addEnemy('ancient', 30000, 150);
+        this.addEnemy('spider_forest', 15000);
+        this.addEnemy('roboost', 20000);
+        this.addEnemy('ancient', 30000);
+
 
         //spawns multiple enemies
         this.spawnInterval = setInterval(() => {
+            // this.addEnemy('ancient', 100000 / (this.spawnedThisWave + 1))
             const min = 0;
             const max = 2;
             let bindex = Math.floor(Math.random() * (max - min + 1)) + min;
             let bobject = this.baddieList[bindex];
-            this.addEnemy(bobject["key"], bobject["duration"], bobject["health"]);
+            this.addEnemy(bobject["key"], bobject["duration"]);
             this.spawnedThisWave++;
         }, 1000);
+        // this.findBaddie(min, max) {
+        // max = Math.floor(max);
+        this.shootInterval = setInterval(() => {
+            for (let i = 0; i < this.placedHeroes.length; i++) {
+                let hero = this.placedHeroes[i];
+                let bullet = this.physics.add.image(hero.x, hero.y, 'green_bullet');
+                bullet.setVelocity(0, -600);
+                for (let j = 0; j < this.spawnedBaddies.length; j++) {
+                    let bad = this.spawnedBaddies[j];
+                    this.physics.add.collider(bullet, bad, () => {
+                        bullet.destroy();
+                        bad.destroy();
+                    });
+                }
+            }
+        }, 500);
 
-        //this places the hero
-        this.input.on('pointerdown', (pointer)=>{
-            // console.log(pointer);
-            this.addHero(pointer.x, pointer.y, this.goodieList[1].key, this.goodieList[1].damage);
+
+
+        //places the hero
+        this.input.on('pointerdown', (pointer) => {
+            this.addHero(pointer.x, pointer.y, this.goodieList[2].key, this.goodieList[2].damage);
         });
-
     }
+
 
     //stops after 30 monsters are created
     update() {
@@ -124,19 +133,24 @@ class TestScene extends Phaser.Scene {
         this.path.lineTo(1400, 900);
     }
 
+    //adds an enemy
     addEnemy(key, duration, health) {
         let follower = this.add.follower(this.path, 96, -32, key);
+        this.physics.add.existing(follower);
+        this.physics.world.enable(follower);
         follower.setData('health', health);
         follower.setScale(0.15);
         follower.startFollow(duration);
+        this.spawnedBaddies.push(follower);
     }
 
+    //adds a hero
     addHero(x, y, key, damage) {
         let hero = this.add.image(x, y, key);
         hero.setData('damage', damage);
         hero.setScale(2);
+        this.placedHeroes.push(hero);
     }
-
     random(min, max) {
 
     }
@@ -171,9 +185,5 @@ class TestScene extends Phaser.Scene {
                 Hero.place(i, j);
             }
         }
-    }
-
-    canPlaceTurret(i, j) {
-        return map[i][j] === 0;
     }
 }
